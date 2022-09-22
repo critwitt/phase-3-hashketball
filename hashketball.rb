418c1f995d2c514d6cd74d1a1110b95e0c3ffb5c
Chris Witte <christopher.t.witte@gmail.com>
2022-09-22 12:42:38 -0400

    commit tree 8a4cd289d490004b6b4bd8af395cc92d7aefc88b

--- hashketball.rb
+++ hashketball.rb
@@ -127,3 +127,24 @@ def game_hash
 end
 
 # Write code here
+def num_points_scored player
+  game_hash.find {|p| p[:player_name] == player}
+end
+
+def shoe_size
+end
+
+def team_colors
+end
+
+def team_names
+end
+
+def player_numbers
+end
+
+def player_stats
+end
+
+def big_shoe_rebounds
+end
